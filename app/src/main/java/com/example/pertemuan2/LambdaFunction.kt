c1874afc18d192868df6850ab1a61a273fd263ed
luthfibphoenix <lutfibahtiar2409@gmail.com>
2024-10-13 19:52:48 +0700

    membuat LambdaFunction

---
+++ app/src/main/java/com/example/pertemuan2/LambdaFunction.kt
@@ -0,0 +1,10 @@
+package com.example.pertemuan2
+
+fun uppercaseString(string: String): String {
+    return string.uppercase()
+}
+
+fun main(){
+    uppercaseString("hello")
+    println(uppercaseString("hello"))
+}
