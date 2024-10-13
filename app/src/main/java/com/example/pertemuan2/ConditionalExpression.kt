6e343a1c1376c33b043b36afe1cadcce1bff7200
luthfibphoenix <lutfibahtiar2409@gmail.com>
2024-10-13 20:01:46 +0700

    membuat fungsi perulangan

--- app/src/main/java/com/example/pertemuan2/ConditionalExpression.kt
+++ app/src/main/java/com/example/pertemuan2/ConditionalExpression.kt
@@ -30,3 +30,29 @@ fun ConditionalStatment(){
         println("Error: Invalid Number Format")
     }
 }
+
+fun Perulangan() {
+    println()
+    println("=== Perulangan ===")
+
+    for (i in 1..5) {
+        println("Perulangan ke-$i")
+    }
+
+    val cars = listOf("BMW", "Toyota", "Honda", "Audi")
+    for (car in cars) {
+        println("Car type: $car")
+    }
+
+    var x = 1
+    while (x <= 5) {
+        println("Perulangan ke-$x")
+        x++
+    }
+
+    var y = 1
+    do {
+        println("Perulangan ke-$y")
+        y++
+    } while (y <= 5)
+}
