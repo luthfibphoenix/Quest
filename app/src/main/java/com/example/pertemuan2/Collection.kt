bbec50fa787d1769b59c45720b7e83d4c5021119
luthfibphoenix <lutfibahtiar2409@gmail.com>
2024-10-13 19:54:46 +0700

    membuat list dengan listof

---
+++ app/src/main/java/com/example/pertemuan2/Collection.kt
@@ -0,0 +1,26 @@
+package com.example.pertemuan2
+
+fun ContohList(){
+    println("=== List ===")
+    //List Read-Only
+    val readOnlyAbjad = listOf("A", "B", "C")
+    println(readOnlyAbjad)
+
+    //List Mutable
+    val shape: MutableList<String> = mutableListOf("Circle", "Square", "Triangle")
+    println(shape)
+
+    //Menambahkan data ke dalam list Mutable
+    shape.add("Circle")
+
+    //Menghapus data dari list Mutable
+    shape.remove("Triangle")
+
+    //Mengubah data di dalam list Mutable
+    shape[0] = "Oval"
+    println(shape)
+
+    //List Read-Only
+    val shapesLocked: List<String> = shape
+    println(shapesLocked)
+}
