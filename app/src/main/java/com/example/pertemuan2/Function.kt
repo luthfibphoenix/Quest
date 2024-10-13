5ca20bf2f8fe6db630c8e9b3f708f8e9958a6351
luthfibphoenix <lutfibahtiar2409@gmail.com>
2024-10-13 20:08:14 +0700

    membuat fungsi argumen

--- app/src/main/java/com/example/pertemuan2/Function.kt
+++ app/src/main/java/com/example/pertemuan2/Function.kt
@@ -10,3 +10,9 @@ fun withParameter(name: String){
     println("=== withParameter ===")
     println("Hello, $name!")
 }
+
+fun withNamedArgument(name: String, age: Int){
+    println()
+    println("=== withNamedArgument ===")
+    println("Hello, $name! You are $age years old.")
+}
